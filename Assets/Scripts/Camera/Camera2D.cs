65e0eac1c6678cd9ca9f1267a5f9cbd83b1b8ab5
Phil <pmasson993@gmail.com>
2016-01-30 17:02:15 -0500

    Camera width scaling

--- Assets/Scripts/Camera/Camera2D.cs
+++ Assets/Scripts/Camera/Camera2D.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class Camera2D : MonoBehaviour 
 {
+    public bool scaleToHeight = true;
+
 	public float pixelsPerUnit = 100.0f;
 	public float zoom = 1.0f;
 
 	public bool updateSizeAutomatically = false;
 	public bool useCustomHeight = true;
 	public float screenHeight = 480.0f;
+
+    public bool useCustomWidth = true;
+    public float screenWidth = 640.0f;
 
   public GameObject followObject = null;
   public float followOffsetX = 0.0f;
@@ -39,16 +44,38 @@ public class Camera2D : MonoBehaviour
 
 	void calculateOrthoSize()
 	{
-		if (m_Camera != null) 
-		{
-			//-- if we are not specifying a manual screen height then use the actual screen height
-			if (useCustomHeight == false) 
-			{
-				screenHeight = Screen.height;
-			}
-			
-			//-- initial camera zoom 1:1 pixel/unit ratio
-			m_Camera.orthographicSize = (screenHeight / pixelsPerUnit * 0.5f ) * zoom;
-		}
+        if (scaleToHeight)
+        {
+            if (m_Camera != null)
+            {
+                //-- if we are not specifying a manual screen height then use the actual screen height
+                if (useCustomHeight == false)
+                {
+                    screenHeight = Screen.height;
+                }
+
+                //-- initial camera zoom 1:1 pixel/unit ratio
+                m_Camera.orthographicSize = (screenHeight / pixelsPerUnit * 0.5f) * zoom;
+            }
+        }
+        else
+        {
+            if (m_Camera != null)
+            {
+                //-- if we are not specifying a manual screen height then use the actual screen height
+                if (useCustomWidth == false)
+                {
+                    screenWidth = Screen.width;
+                }
+
+                float screenAspectRatio = m_Camera.aspect;
+                float heightOfScreen = screenWidth / screenAspectRatio;
+                //-- initial camera zoom 1:1 pixel/unit ratio
+                m_Camera.orthographicSize = (heightOfScreen / pixelsPerUnit * 0.5f) * zoom;
+
+                //Setting screen position (offset)
+                m_Camera.transform.position = new Vector3(screenWidth / 2 / pixelsPerUnit, heightOfScreen / 2 / pixelsPerUnit, -10.0f);
+            }
+        }
 	}
 }
