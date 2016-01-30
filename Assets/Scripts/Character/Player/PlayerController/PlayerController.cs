ec4095450be94093b5e9041ae94f3679df21378d
juanwinsor <juan.winsor@gmail.com>
2016-01-30 12:51:42 -0500

    -ik rig test -kenney test assets

--- Assets/Scripts/Character/Player/PlayerController/PlayerController.cs
+++ Assets/Scripts/Character/Player/PlayerController/PlayerController.cs
@@ -29,6 +29,29 @@ public class PlayerController : MonoBehaviour
 
     public void SetPlayerState( PlayerState state )
     {
-
+        //-- this will trigger movement and animations on the character
+        switch( state )
+        {
+            case PlayerState.Bump_Up:
+                break;
+            case PlayerState.Bump_Down:
+                break;
+            case PlayerState.Bump_Left:
+                break;
+            case PlayerState.Bump_Right:
+                break;
+            case PlayerState.Hop_Down:
+                break;
+            case PlayerState.Hop_Up:
+                break;
+            case PlayerState.Hop_Left:
+                break;
+            case PlayerState.Hop_Right:
+                break;
+            case PlayerState.Slide:
+                break;
+            case PlayerState.Die:
+                break;
+        }
     }
 }
