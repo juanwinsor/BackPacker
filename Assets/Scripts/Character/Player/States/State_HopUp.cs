e3eeb1fd52bffdb3b4b0269827f100ce7ed8133d
juanwinsor <juan.winsor@gmail.com>
2016-01-30 18:49:19 -0500

    - character states

---
+++ Assets/Scripts/Character/Player/States/State_HopUp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class State_HopUp : StateMachineBehaviour
+{
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
