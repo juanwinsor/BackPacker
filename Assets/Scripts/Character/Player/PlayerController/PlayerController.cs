ae086f7e944fb6eef096a7a19d5356ac44aa5518
juanwinsor <juan.winsor@gmail.com>
2016-01-30 01:24:47 -0500

    - added player controller, session manager, gameplay manager basic structure

---
+++ Assets/Scripts/Character/Player/PlayerController/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MoveDirection
+{
+
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //-- give the gameplay manager a reference to the player controller
+        GameplayManager.Instance.RegisterPlayerController( this );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void RequestMove( MoveDirection direction )
+    {
+        GameplayManager.Instance.RequestMove( direction );
+    }
+
+    public void SetPlayerState( PlayerState state )
+    {
+
+    }
+}
---
+++ Assets/Scripts/Managers/GameSession/GameSessionManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSessionManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
