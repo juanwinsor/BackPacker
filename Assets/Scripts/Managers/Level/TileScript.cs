808bdfea214d1816528be1e1cab078fbbd2f5b0e
Phil <pmasson993@gmail.com>
2016-01-30 16:30:04 -0500

    Level now spawns 4 tiles

--- Assets/Scripts/Managers/Level/TileScript.cs
+++ Assets/Scripts/Managers/Level/TileScript.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-enum TileType { Safe, Slide, Spike }
-enum LaneNumber {Left, LeftCenter, RightCenter, Right}
+public enum TileType { Safe, Slide, Spike }
+public enum LaneNumber {Left, LeftCenter, RightCenter, Right}
 
 public class TileScript : MonoBehaviour
 {
     TileType myTileType; //The tile type that was given by the initilization from the manager when setting the sprite.
-    LaneNumber myLaneNumber; //What lane the tile is in (right, center or left)
+    public LaneNumber myLaneNumber; //What lane the tile is in (right, center or left)
     int myElevation = 0; //what elevation the tile is.
+    Sprite sprite;
 
     // Use this for initialization
     void Start()
@@ -24,10 +25,33 @@ public class TileScript : MonoBehaviour
 
     public void SetNewTile(TileType tileType, LaneNumber laneNumber, int elevation, Sprite theSprite)
     {
+        sprite = theSprite;
         myTileType = tileType;
         myLaneNumber = laneNumber;
         myElevation = elevation;
-        gameObject.GetComponent<SpriteRenderer>().sprite = theSprite;
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
     }//End SetNewTile
 
+    public void setTilePosition(int currElevation)
+    {
+        float x = 0.0f;
+        float y = 0.0f;
+        switch (myLaneNumber)
+        {
+            case LaneNumber.Left:
+                x = sprite.textureRect.width / 2;
+                break;
+            case LaneNumber.LeftCenter:
+                x = sprite.textureRect.width + sprite.textureRect.width / 2;
+                break;
+            case LaneNumber.RightCenter:
+                x = sprite.textureRect.width * 2 + sprite.textureRect.width / 2;
+                break;
+            case LaneNumber.Right:
+                x = sprite.textureRect.width * 3 + sprite.textureRect.width / 2;
+                break;
+        }
+        x = x / sprite.pixelsPerUnit;
+        gameObject.transform.position = new Vector3(x, y, 0.0f);
+    }
 }
