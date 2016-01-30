02266615d387ba7e331d90c088ca117f3e7d164d
Phil <pmasson993@gmail.com>
2016-01-30 18:45:48 -0500

    TIles now spawn all safe tiles properly scaled.

--- Assets/Scripts/Managers/Level/TileScript.cs
+++ Assets/Scripts/Managers/Level/TileScript.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public enum TileType { Safe, Slide, Spike }
 public enum LaneNumber {Left, LeftCenter, RightCenter, Right}
 
-public class TileScript : MonoBehaviour
+public class TileScript : MonoBehaviour, IPoolable
 {
     TileType myTileType; //The tile type that was given by the initilization from the manager when setting the sprite.
     public LaneNumber myLaneNumber; //What lane the tile is in (right, center or left)
     int myElevation = 0; //what elevation the tile is.
     Sprite sprite;
 
+    float spriteScaledWidth;
+    float spriteScaledHeight;
+
     // Use this for initialization
     void Start()
     {
+        
+    }
 
+    //initialization from Objectpool
+    public void Initialize ()
+    {
+        //float spriteScale = 160.0f/256.0f;
+        //gameObject.transform.localScale = new Vector3(spriteScale, spriteScale, spriteScale);
     }
 
     // Update is called once per frame
@@ -30,6 +40,9 @@ public class TileScript : MonoBehaviour
         myLaneNumber = laneNumber;
         myElevation = elevation;
         gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+
+        spriteScaledWidth = 160.0f / 256.0f * sprite.textureRect.width;
+        spriteScaledHeight = spriteScaledWidth;
     }//End SetNewTile
 
     public void setTilePosition(int currElevation)
@@ -39,18 +52,19 @@ public class TileScript : MonoBehaviour
         switch (myLaneNumber)
         {
             case LaneNumber.Left:
-                x = sprite.textureRect.width / 2;
+                x = spriteScaledWidth / 2;
                 break;
             case LaneNumber.LeftCenter:
-                x = sprite.textureRect.width + sprite.textureRect.width / 2;
+                x = spriteScaledWidth + spriteScaledWidth / 2;
                 break;
             case LaneNumber.RightCenter:
-                x = sprite.textureRect.width * 2 + sprite.textureRect.width / 2;
+                x = spriteScaledWidth * 2 + spriteScaledWidth / 2;
                 break;
             case LaneNumber.Right:
-                x = sprite.textureRect.width * 3 + sprite.textureRect.width / 2;
+                x = spriteScaledWidth * 3 + spriteScaledWidth / 2;
                 break;
         }
+        y = (spriteScaledHeight * (myElevation - currElevation) + spriteScaledHeight / 2) / sprite.pixelsPerUnit;
         x = x / sprite.pixelsPerUnit;
         gameObject.transform.position = new Vector3(x, y, 0.0f);
     }
